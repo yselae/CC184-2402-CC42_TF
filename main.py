59330f2e7e9f4c7c62e512aaf5f86f93fd5bf949
Ale <145732277+alefs9@users.noreply.github.com>
2024-11-17 20:30:44 -0500

    Arreglar diseño

--- main.py
+++ main.py
@@ -185,20 +185,20 @@ def iniciar_interfaz():
         ventana, text="Bienvenido a Optimización de Rutas", font=("Helvetica", 24, "bold"),
         foreground='white', background='grey'
     )
-    titulo_label.place(relx=0.5, y=20, anchor='center') #.place(x=270, )
+    titulo_label.place(x=170, y=20)
 
     frame_campos = tk.Frame(ventana, bg='grey')
     frame_campos.place(relx=0.5, rely=0.15, anchor='n')
 
     # Etiqueta y ComboBox para el paradero de origen
     ttk.Label(ventana, text="Paradero de origen:", foreground='white', background='grey').place(x=50, y=100)
-    origen_combo = ttk.Combobox(ventana, values=paraderos["Nombre"].tolist(), width=30)
-    origen_combo.place(x=200, y=100)
+    origen_combo = ttk.Combobox(ventana, values=paraderos["Nombre"].tolist(), width=35)
+    origen_combo.place(x=180, y=100)
 
     # Etiqueta y ComboBox para el paradero de destino
     ttk.Label(ventana, text="Paradero de destino:", foreground='white', background='grey').place(x=50, y=140)
-    destino_combo = ttk.Combobox(ventana, values=paraderos["Nombre"].tolist(), width=30)
-    destino_combo.place(x=200, y=140)
+    destino_combo = ttk.Combobox(ventana, values=paraderos["Nombre"].tolist(), width=35)
+    destino_combo.place(x=180, y=140)
 
     # Botón para encontrar la ruta
     find_route_button = tk.Button(
@@ -209,24 +209,24 @@ def iniciar_interfaz():
         font=("Helvetica", 10, "bold"),
         command=lambda: calcular_minima_distancia(origen_combo.get(), destino_combo.get())
     )
-    find_route_button.place(x=150, y=200, width=200, height=45)
+    find_route_button.place(x=150, y=190, width=160, height=45)
 
     # Sección para agregar un nuevo punto
-    ttk.Label(ventana, text="Agregar Punto", font=("Helvetica", 14, "bold"), foreground='white', background='grey').place(x=580, y=80)
+    ttk.Label(ventana, text="Agregar Punto", font=("Helvetica", 14, "bold"), foreground='white', background='grey').place(x=600, y=100)
 
     # Campos para agregar un nuevo punto
-    ttk.Label(ventana, text="Nombre:", foreground='white', background='grey').place(x=480, y=130)
+    ttk.Label(ventana, text="Nombre:", foreground='white', background='grey').place(x=520, y=150)
     name_entry = tk.Entry(ventana, width=30)
-    name_entry.place(x=580, y=130)
+    name_entry.place(x=580, y=150)
 
     # Entradas para coordenadas X y Y en una misma línea
     ttk.Label(ventana, text="X:", foreground='white', background='grey').place(x=480, y=170)
-    x_entry = tk.Entry(ventana, width=10)
-    x_entry.place(x=510, y=170)
+    x_entry = tk.Entry(ventana)
+    x_entry.place(x=510, y=190)
 
     ttk.Label(ventana, text="Y:", foreground='white', background='grey').place(x=670, y=170)
-    y_entry = tk.Entry(ventana, width=10)
-    y_entry.place(x=700, y=170)
+    y_entry = tk.Entry(ventana)
+    y_entry.place(x=700, y=190)
 
     # Botón para agregar un nuevo punto
     add_button = tk.Button(
@@ -237,7 +237,7 @@ def iniciar_interfaz():
         fg="white",
         command=lambda: solicitar_nuevo_nodo(name_entry.get(), x_entry.get(), y_entry.get())
         )
-    add_button.place(x=580, y=220, width=200, height=45)
+    add_button.place(x=50, y=400, width=200, height=40)
 
     # Botones adicionales para otras funcionalidades
     button_frame = tk.Frame(ventana, bg='grey')
@@ -248,12 +248,16 @@ def iniciar_interfaz():
     button_height = 2
 
     # Botones organizados en filas y columnas con colores de fondo correspondientes y sus respectivas funcionalidades
-    tk.Button(button_frame, text="MOSTRAR MAPA", bg="#90e0ef", fg="white", width=button_width, height=button_height, command=mostrar_mapa).place(x=10, y=10)
-    tk.Button(button_frame, text="MOSTRAR PUNTOS DE RECARGA", bg="#ffd60a", fg="white", width=button_width, height=button_height, command=mostrar_puntos_recarga).place(x=250, y=10)
-    tk.Button(button_frame, text="MOSTRAR GRAFO", bg="#ff6b6b", fg="white", width=button_width, height=button_height, command=mostrar_ventana_seleccion_grafo).place(x=490, y=10)
-    tk.Button(button_frame, text="PARADEROS MORADO", bg="#9c27b0", fg="white", width=button_width, height=button_height, command=lambda: mostrar_paraderos_por_color("Morado", "purple")).place(x=10, y=70)
-    tk.Button(button_frame, text="PARADEROS ROJO", bg="#e63946", fg="white", width=button_width, height=button_height, command=lambda: mostrar_paraderos_por_color("Rojo", "red")).place(x=250, y=70)
-    tk.Button(button_frame, text="PARADEROS AZUL", bg="#1e3a8a", fg="white", width=button_width, height=button_height, command=lambda: mostrar_paraderos_por_color("Azul", "blue")).place(x=490, y=70)
+    tk.Button(button_frame, text="MOSTRAR MAPA", bg="#90e0ef", fg="white", width=button_width, height=button_height, command=mostrar_mapa).grid(row=1, column=0, padx=10, pady=10, sticky="ew")
+    tk.Button(button_frame, text="MOSTRAR PUNTOS DE RECARGA", bg="#ffd60a", fg="white", width=button_width, height=button_height, command=mostrar_puntos_recarga).grid(row=2, column=0, padx=10, pady=10, sticky="ew")
+    tk.Button(button_frame, text="MOSTRAR GRAFO", bg="#ff6b6b", fg="white", width=button_width, height=button_height, command=mostrar_ventana_seleccion_grafo).grid(row=3, column=0, padx=10, pady=10, sticky="ew")
+    tk.Button(button_frame, text="PARADEROS MORADO", bg="#9c27b0", fg="white", width=button_width, height=button_height, command=lambda: mostrar_paraderos_por_color("Morado", "purple")).grid(row=1, column=1, padx=10, pady=10, sticky="ew")
+    tk.Button(button_frame, text="PARADEROS ROJO", bg="#e63946", fg="white", width=button_width, height=button_height, command=lambda: mostrar_paraderos_por_color("Rojo", "red")).grid(row=2, column=1, padx=10, pady=10, sticky="ew")
+    tk.Button(button_frame, text="PARADEROS AZUL", bg="#1e3a8a", fg="white", width=button_width, height=button_height, command=lambda: mostrar_paraderos_por_color("Azul", "blue")).grid(row=3, column=1, padx=10, pady=10, sticky="ew")
+
+    button_frame.grid_columnconfigure(0, weight=1)
+    button_frame.grid_columnconfigure(1, weight=1)
+
 
     ventana.mainloop()
 
